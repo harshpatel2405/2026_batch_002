0393e76f15cd5733b9b901def7548fa2f74de97b
harshpatel2405 <harshpatel4504@gmail.com>
2026-05-10 14:09:44 +0530

    datatype and dialogboxes and if_else

---
+++ conditionals/if.js
@@ -0,0 +1,20 @@
+let age = 19;
+
+if (age > 18)
+    console.log("You can vote")
+
+let num = parseInt(45.56);
+console.log(num)
+if(num > 0)
+    console.log("Number is positive. ")
+
+// ^ Task : Enter a character. If it is a digit (0–9), print "It is a digit"  
+
+/*
+^   13) Input weight(in kg) and height(in meters) → calculate BMI and classify:
+^   	BMI = weight / (height * height)
+^   		BMI < 18.5 → Underweight
+^   		18.5–24.9 → Normal
+^   		25–29.9 → Overweight
+^   		30+ → Obese
+*/
